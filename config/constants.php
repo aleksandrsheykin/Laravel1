c00ab0f91a0afa64ec17a762ea473f0795852b74
aleksandrsheykin <Lunatik_@bk.ru>
2016-09-16 14:37:35 +0500

    delete con

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'VK_CLIENT_ID' => '5304108',  
-    'VK_CLIENT_SECRET' => 'idXTNCB7wRRxw2NTCWo5',
-    'VK_CLIENT_URI' => 'http://laravel1/',   
+    'VK_CLIENT_ID' => '',  
+    'VK_CLIENT_SECRET' => '',
+    'VK_CLIENT_URI' => '',
 ];
