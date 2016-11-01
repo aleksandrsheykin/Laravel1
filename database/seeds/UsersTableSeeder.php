16de5754a67b388a0bdb714b6e7affa8c6b4c825
Шейкин Александр Сергеевич <prog02@intser.ru>
2016-11-01 17:47:17 +0500

    add seed admin user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'firstname' => 'Саня',
+            'lastname' => 'Шейкин',
+            'email' => '6125131@vk.com',
+			'password' => bcrypt(str_random(20)),
+			'vk_id' => '6125131'
+        ]);
+    }
+}
